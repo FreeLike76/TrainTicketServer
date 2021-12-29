6be27bdf9507b744a54133ae03b099ed667089ee
Dmytro Geleshko <dmytro.geleshko01@gmail.com>
2021-12-29 23:37:58 +0200

    changed push to put

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ block_ticket_args = reqparse.RequestParser()
 block_ticket_args.add_argument("id", type=str, help="Id is required to block ticket", required=True)
 block_ticket_args.add_argument("status", type=str, help="Whether the ticket is blocked", required=True)
 
+
 class TTSResource(Resource):
     def get(self):
         if len(request.args) == 0:
@@ -15,7 +16,7 @@ class TTSResource(Resource):
         else:
             return server.get_by(request.args).to_dict(orient="records")
 
-    def post(self):
+    def put(self):
         args = block_ticket_args.parse_args()
         try:
             server.set_status(args["id"], args["status"])
